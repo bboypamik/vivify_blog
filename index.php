08a05ff1c3babfc7298f7024217061bbefce1a17
Miroslav Pantos <bboypamik@gmail.com>
2018-01-31 21:01:50 +0100

    zadatak 3

--- index.php
+++ index.php
@@ -13,9 +13,10 @@
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-
+     
     <!-- Custom styles for this template -->
     <link href="styles/blog.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 
 <body>
@@ -29,7 +30,7 @@
         <div class="col-sm-8 blog-main">
 
             <div class="blog-post">
-                <h2 class="blog-post-title">Sample blog post</h2>
+                <a href =""> <h2 class="blog-post-title">Sample blog post</h2></a>
                 <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
 
                 <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
@@ -39,13 +40,13 @@
                     <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
                 </blockquote>
                 <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-                <h2>Heading</h2>
+                 <a href =""><h2>Heading</h2></a>
                 <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-                <h3>Sub-heading</h3>
+                 <a href =""><h3>Sub-heading</h3></a>
                 <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
                 <pre><code>Example code block</code></pre>
                 <p>Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-                <h3>Sub-heading</h3>
+                 <a href =""><h3>Sub-heading</h3></a>
                 <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
                 <ul>
                     <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
@@ -62,7 +63,7 @@
             </div><!-- /.blog-post -->
 
             <div class="blog-post">
-                <h2 class="blog-post-title">Another blog post</h2>
+                 <a href =""><h2 class="blog-post-title">Another blog post</h2></a>
                 <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
 
                 <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
@@ -74,7 +75,7 @@
             </div><!-- /.blog-post -->
 
             <div class="blog-post">
-                <h2 class="blog-post-title">New feature</h2>
+                 <a href =""><h2 class="blog-post-title">New feature</h2></a>
                 <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
 
                 <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
