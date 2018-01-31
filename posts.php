550835b94f628e26a518260cdc6e7d10abc21513
Miroslav Pantos <bboypamik@gmail.com>
2018-01-31 21:57:58 +0100

    zadatak 4

--- posts.php
+++ posts.php
@@ -1,42 +1,51 @@
 
+   <?php
+       $servername = "127.0.0.1";
+       $username = "root";
+       $password = "vivify";
+       $dbname ="vivify_posts";
                
-               <?php include "header.php";?>
+    try {
+       $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+       // set the PDO error mode to exception
+       $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   }
+   catch(PDOException $e)
+   {
+       echo $e->getMessage();
+   }
 
-          
-            <div class="blog-post">
-                <h2 class="blog-post-title">Sample blog post</h2>
-                <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+                // pripremamo upit
 
-                <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
-                <hr>
-                <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-                <blockquote>
-                    <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                </blockquote>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-                <h2>Heading</h2>
-                <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-                <h3>Sub-heading</h3>
-                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                <pre><code>Example code block</code></pre>
-                <p>Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-                <h3>Sub-heading</h3>
-                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                <ul>
-                    <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-                    <li>Donec id elit non mi porta gravida at eget metus.</li>
-                    <li>Nulla vitae elit libero, a pharetra augue.</li>
-                </ul>
-                <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
-                <ol>
-                    <li>Vestibulum id ligula porta felis euismod semper.</li>
-                    <li>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</li>
-                    <li>Maecenas sed diam eget risus varius blandit sit amet non magna.</li>
-                </ol>
-                <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.</p>
-            </div><!-- /.blog-post -->
+                $sql = "SELECT id, title, body, author, created_at FROM posts ORDER BY created_at DESC LIMIT 3";
 
-            <div class="blog-post">
+                $statement = $connection->prepare($sql);
+
+                // izvrsavamo upit
+
+                $statement->execute();
+
+                // zelimo da se rezultat vrati kao asocijativni niz.
+
+                // ukoliko izostavimo ovu liniju, vratice nam se obican, numerisan niz
+
+                $statement->setFetchMode(PDO::FETCH_ASSOC);
+
+                // punimo promenjivu sa rezultatom upita
+
+                $posts = $statement->fetchAll();
+
+                // koristite var_dump kada god treba da proverite sadrzaj neke promenjive
+
+                    echo '<pre>';
+
+                    var_dump($posts);
+
+                    echo '</pre>';
+
+            ?>
+
+              
 
                 <?php include "sidebar.php"; ?>
        <?php include "footer.php";?> 
